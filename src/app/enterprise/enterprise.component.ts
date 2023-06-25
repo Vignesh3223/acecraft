a8db4c1cde8060e65f7622d1df245a28d29e23a0
Vignesh3223 <viketvicky@gmail.com>
2023-06-25 13:45:21 +0530

    25/06/23

--- src/app/enterprise/enterprise.component.ts
+++ src/app/enterprise/enterprise.component.ts
@@ -4,6 +4,7 @@ interface EventItem {
   id?: number;
   status?: string;
   image?: string;
+  color?: string;
 }
 
 @Component({
@@ -16,12 +17,12 @@ export class EnterpriseComponent {
 
   constructor() {
     this.events = [
-      { id: 1, status: 'Single line brief', image: '/assets/images/icon-1.png' },
-      { id: 2, status: ' Mind mapping', image: '/assets/images/icon-2.png' },
-      { id: 3, status: 'Design and details', image: '/assets/images/icon-3.png' },
-      { id: 4, status: 'Sampling', image: '/assets/images/icon-4.png' },
-      { id: 5, status: 'Manufacturing', image: '/assets/images/icon-5.png' },
-      { id: 6, status: 'Delivery', image: '/assets/images/icon-6.png' },
+      { id: 1, status: 'Single line brief', image: '/assets/images/icon-1.png', color: '#978F8F' },
+      { id: 2, status: 'Mind mapping', image: '/assets/images/icon-2.png', color: '#978F8F' },
+      { id: 3, status: 'Design and details', image: '/assets/images/icon-3.png', color: '#978F8F' },
+      { id: 4, status: 'Sampling', image: '/assets/images/icon-4.png', color: '#978F8F' },
+      { id: 5, status: 'Manufacturing', image: '/assets/images/icon-5.png', color: '#978F8F' },
+      { id: 6, status: 'Delivery', image: '/assets/images/icon-6.png', color: '#978F8F' },
     ];
   }
 }
--- src/services/ace-service.service.ts
+++ src/services/ace-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Notes} from 'src/models/acecraft';
+import { Notes } from 'src/models/acecraft';
 
 
 @Injectable({
@@ -10,10 +10,11 @@ export class AceServiceService {
 
   constructor(private http: HttpClient) { }
 
-  noteurl:string = "http://localhost:3000/notes";
- 
+  noteurl: string = "http://localhost:3000/notes";
+  
+
   getNotes() {
     return this.http.get<Notes[]>(this.noteurl);
   }
-
+ 
 }
