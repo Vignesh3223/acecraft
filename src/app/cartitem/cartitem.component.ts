18a632e2fe473887d01796b23a558cf3f1df2269
Vignesh3223 <viketvicky@gmail.com>
2023-07-06 15:58:05 +0530

    06/07/23

--- src/app/cartitem/cartitem.component.ts
+++ src/app/cartitem/cartitem.component.ts
@@ -12,10 +12,74 @@ import Swal from 'sweetalert2';
 })
 export class CartitemComponent implements OnInit {
 
+  paymentHandler: any = null;
+
+  makePayment(amount: any) {
+    const paymentHandler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_51NQoOgSIF8NYMtoSsE3ybqgFcjiafBmE6SgRG0LOoz02qHSXlYDYWfB0wcZzauNoi8fpI8CJ7WQCOqcIeBJG72Pf00sQlgKl12',
+      locale: 'auto',
+      token: function (stripeToken: any) {
+        console.log(stripeToken);
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+        });
+
+        Toast.fire({
+          icon: 'success',
+          title: 'Order Placed Successfully',
+          background: '#E6F9ED',
+          color: '#006730'
+        });
+        this.cartserv.removeItemFromCart();
+      }
+    });
+    paymentHandler.open({
+      name: 'Order',
+      description: 'Order Details',
+      amount: amount,
+    });
+  }
+
+  invokeStripe() {
+    if (!window.document.getElementById('stripe-script')) {
+      const script = window.document.createElement('script');
+      script.id = 'stripe-script';
+      script.type = 'text/javascript';
+      script.src = 'https://checkout.stripe.com/checkout.js';
+      script.onload = () => {
+        this.paymentHandler = (<any>window).StripeCheckout.configure({
+          key: 'pk_test_51NQoOgSIF8NYMtoSsE3ybqgFcjiafBmE6SgRG0LOoz02qHSXlYDYWfB0wcZzauNoi8fpI8CJ7WQCOqcIeBJG72Pf00sQlgKl12',
+          locale: 'auto',
+          token: function (stripeToken: any) {
+            console.log(stripeToken);
+            const Toast = Swal.mixin({
+              toast: true,
+              position: 'top',
+              showConfirmButton: false,
+              timer: 3000,
+              timerProgressBar: true,
+            });
+            Toast.fire({
+              icon: 'error',
+              title: 'Error in generating Stripe Payment Gateway',
+              background: '#FCEBE9',
+              color: '#751A0C'
+            });
+          }
+        });
+      }
+      window.document.body.appendChild(script);
+    }
+  }
+
   constructor(private cartserv: CartserviceService, private router: Router) { }
 
   carts: Cart = {
-    id:0,
+    id: 0,
     text: '',
     image: '',
     size: 1,
@@ -83,6 +147,7 @@ export class CartitemComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.invokeStripe();
     this.cartserv.getCartItems().subscribe(
       (response) => {
         this.cart = response;
